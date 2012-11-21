88191028ca3875f51f1a3fd91b666f439271fb96
Patrick Landis <pal25@case.edu>
2012-11-21 16:36:53 -0500

    Got the run files to work properly."

--- netbench/run.py
+++ netbench/run.py
@@ -1,8 +1,22 @@
 from udpprobe import UDPProbe
 from dispatcher import EventLoop
+import getopt, sys
+import argparse
 
-def main(addr=''):
-	port = 50003
+def parseargs():
+	parser = argparse.ArgumentParser(description="NetBench: A tool to benchmark router hops and TTL")
+	parser.add_argument("address", type=str, help="address to benchmark, defaults to localhost", default='127.0.0.1')
+	parser.add_argument("--output", help="output file, defaults to stdout")
+	parser.add_argument("--port", type=int, help="port number to probe, defaults to 50003", default=50003)
+	
+	args = parser.parse_args()
+
+	if args.output:
+		sys.stdout = open(args.output, 'w')
+
+	run(args.address, args.port)
+
+def run(addr, port):
 	probe = UDPProbe(addr, port)
 
 	loop = EventLoop()
@@ -10,5 +24,5 @@ def main(addr=''):
 	loop.run(timeout = 0.1)
 
 if __name__ == '__main__':
-	main(addr = 'google.com')
+	parseargs()
 	
